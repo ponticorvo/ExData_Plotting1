cf1de6cf52920a02e797fa4b5235029752458200
Ponticorvo <Ponticorvo@gmail.com>
2015-06-07 15:08:06 -0700

    Cleaned typos

--- Plot4.R
+++ Plot4.R
@@ -14,23 +14,7 @@ startDate <- as.Date("2007-02-01")
 stopDate <- as.Date("2007-02-02")
 df.sub2 <- df[df$Date >= startDate & df$Date <=stopDate,]
   
-#Plot a histogram of the Global Active power data
-png("plot1.png", width = 480, height = 480)
-hist(df.sub$Global_active_power, main = "Global Active power", col = "red", xlab = "Global Active Power (kilowatts)", )
-dev.off()
-
-png("plot2.png", width = 480, height = 480)
-plot(df.sub$Time, df.sub$Global_active_power, xlab = "", ylab = "Global Active Power (kilowatts)", type = "l")
-dev.off()
-
-png("plot3.png", width = 480, height = 480)
-plot(x=df.sub$Time,y=df.sub$Sub_metering_1,type="l", xlab="",ylab="Energy Sub Meetering")
-lines(x=df.sub$Time,y=df.sub$Sub_metering_2, type="l",col="red")
-lines(x=df.sub$Time,y=df.sub$Sub_metering_3, type="l",col="blue")
-legendTxt <-c("Sub Meetering 1","Sub Meetering 2", "Sub Meetering 3")
-legend("topright",c("Sub Metering 1","Sub Metering 2", "Sub Metering 3"),lty=c(1,1,1),lwd=c(2.5,2.5,2.5),col=c("black","blue","red"))
-dev.off()
-
+#Plot multiple plots, save to one figure, and save to a png
 png(filename = "plot4.png", width = 480, height = 480)
 par(mfrow=c(2,2))
 
@@ -38,10 +22,10 @@ plot(df.sub$Time,df.sub$Global_active_power,type="l", xlab="",ylab="Global Activ
 
 plot(df.sub$Time,df.sub$Voltage,type="l", xlab="datetime",ylab="Voltage")
 
-plot(x=df.sub$Time,y=df.sub$Sub_metering_1,type="l", xlab="",ylab="Energy Sub Meetering")
+plot(x=df.sub$Time,y=df.sub$Sub_metering_1,type="l", xlab="",ylab="Energy Sub Metering")
 lines(x=df.sub$Time,y=df.sub$Sub_metering_2, type="l",col="red")
 lines(x=df.sub$Time,y=df.sub$Sub_metering_3, type="l",col="blue")
-legendTxt <-c("Sub Meetering 1","Sub Meetering 2", "Sub Meetering 3")
+legendTxt <-c("Sub Metering 1","Sub Metering 2", "Sub Metering 3")
 legend("topright",c("Sub Metering 1","Sub Metering 2", "Sub Metering 3"),lty=c(1,1,1),lwd=c(2.5,2.5,2.5),col=c("black","blue","red"))
 
 plot(df.sub$Time,df.sub$Global_reactive_power,type="l", xlab="datetime",ylab="Global_reactive_power")
